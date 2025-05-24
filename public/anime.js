8ab20fc96371866b169af76e66085d34c274db49
Marc Heinrich <marc.heinrich@free.fr>
2025-05-24 22:47:32 +0200

    Add a button to add a new suggested anime
    Add a button in the anime page to allow to enter an url/malID to insert into the database.

--- public/anime.js
+++ public/anime.js
@@ -1,41 +1,18 @@
-import {db, parseResult, loadVideos, reloadDB} from "./common.js"
+import {sendRequest, reloadDB} from "./common.js"
 
-const channelVideoList = document.getElementById("channelVideoList");
-const channelDisplayNewOnly = document.getElementById("channelDisplayNewOnly");
+const newAnimeButton = document.getElementById("newAnimeButton");
+const newAnimeField = document.getElementById("newAnimeField");
+const recommendedAnimeList = document.getElementById("recommendedAnimeList");
+const watchedAnimeList = document.getElementById("watchedAnimeList");
 
-var currentChannelInfo = null;
-
-async function fetchChannelInfos()
-{
-	const currentChannelId = new URLSearchParams(window.location.search).get("id");
-
-	for (var sub of parseResult(db.exec("SELECT * FROM Subscriptions"))) {
-		if (sub.Id == currentChannelId)
-		{
-			currentChannelInfo = sub;
-			break;
-		}
-	}
-	if (currentChannelInfo == null)
-	{
-		document.querySelector("h1").textContent = "Channel not found";
-	}
-	document.querySelector("#channelPage h1").textContent = currentChannelInfo.Title;
-}
-
-channelDisplayNewOnly.onclick = async function() {
-	if (channelDisplayNewOnly.checked) {
-		await loadVideos(channelVideoList, `SELECT * FROM Videos WHERE SubscriptionId = ${currentChannelInfo.Id} AND Viewed = FALSE`);
-	}
-	else {
-		await loadVideos(channelVideoList, `SELECT * FROM Videos WHERE SubscriptionId = ${currentChannelInfo.Id}`);
-	}
-}
+newAnimeButton.onclick = async function() {
+	await sendRequest("/videomiam/addAnime", {
+		malId : newAnimeField.value,
+	});
+};
 
 async function main() {
 	await reloadDB();
-	await fetchChannelInfos();
-	await loadVideos(channelVideoList, `SELECT * FROM Videos WHERE SubscriptionId = ${currentChannelInfo.Id}`, false);
 }
 
 main();
