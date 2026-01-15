0b235af1c3e406eb04cc1bfe605699851048d99b
Marc Heinrich <marc.heinrich@free.fr>
2026-01-15 22:18:53 +0100

    Added a 'recent' section in the main video page

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 import {reloadDB, loadVideos} from "./common.js"
 
 const videoList = document.getElementById("videoList");
+const recentVideoList = document.getElementById("recentVideoList");
 
 async function main() {
 	await reloadDB();
 	await loadVideos(videoList, "SELECT * FROM Videos WHERE Viewed = FALSE;", true);
+	await loadVideos(recentVideoList, "SELECT * FROM Videos INNER JOIN Subscriptions ON Videos.SubscriptionId = Subscriptions.Id WHERE Viewed = FALSE AND Subscriptions.IsFavorite = FALSE ORDER BY Videos.UploadDate DESC LIMIT 10;");
 }
 
 main();
