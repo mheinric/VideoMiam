773a35deeb8a5eb0bcab215b052479764961fcd5
Marc Heinrich <marc.heinrich@free.fr>
2026-01-19 18:33:35 +0100

    Fixing issue in SQL query for recent videos

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ const recentVideoList = document.getElementById("recentVideoList");
 async function main() {
 	await reloadDB();
 	await loadVideos(videoList, "SELECT * FROM Videos WHERE Viewed = FALSE;", true);
-	await loadVideos(recentVideoList, "SELECT * FROM Videos INNER JOIN Subscriptions ON Videos.SubscriptionId = Subscriptions.Id WHERE Viewed = FALSE AND Subscriptions.IsFavorite = FALSE ORDER BY Videos.UploadDate DESC LIMIT 10;");
+	await loadVideos(recentVideoList, "SELECT Videos.* FROM Videos INNER JOIN Subscriptions ON Videos.SubscriptionId = Subscriptions.Id WHERE Viewed = FALSE AND Subscriptions.IsFavorite = FALSE ORDER BY Videos.UploadDate DESC LIMIT 10;");
 }
 
 main();
