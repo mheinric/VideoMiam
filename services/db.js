0da064b29058928c31b86af9e099b67c5e63aad7
Marc Heinrich <marc.heinrich@free.fr>
2026-02-21 14:21:05 +0100

    Refactor a little bit the code in db.js
    Add a function to automatically cache prepared statements to avoid having to define variables each time.

--- services/db.js
+++ services/db.js
@@ -9,6 +9,14 @@ if (!dbExists) {
     db.exec(fs.readFileSync('databaseSchema.sql', 'utf-8'));
 }
 
+const statementCache = new Map();
+function prepare(statement) {
+    if (!statementCache.has(statement)) {
+        statementCache.set(statement, db.prepare(statement));
+    }
+    return statementCache.get(statement)
+}
+
 export async function clearDB() {
     //Note: be careful of order to account for foreign key dependencies.
     //Note: also delete elements from sqlite_sequence to restart id numbering from 0.
@@ -25,39 +33,35 @@ export async function clearDB() {
     `);
 }
 
-const listSubStatement = db.prepare("SELECT * FROM Subscriptions");
-const listSubByUserStatement = db.prepare(`
-    SELECT Subscriptions.* 
-    FROM Subscriptions INNER JOIN UserSubscriptions ON Subscriptions.Id = UserSubscriptions.ChannelId
-    WHERE UserId = ?`);
 export async function getAllSubscriptions(userId = null) {
     if (userId === null) {
-        return listSubStatement.all();
+        return prepare("SELECT * FROM Subscriptions").all();
     }
     else {
-        return listSubByUserStatement.all(userId);
+        return prepare(`
+            SELECT Subscriptions.* 
+            FROM Subscriptions INNER JOIN UserSubscriptions ON Subscriptions.Id = UserSubscriptions.ChannelId
+            WHERE UserId = ?
+        `).all(userId);
     }
 }
 
-const subDetailsStatement = db.prepare("SELECT * FROM Subscriptions WHERE Id = ?");
 export async function getSubscription(channelId) {
-    return subDetailsStatement.get(channelId);
+    return prepare("SELECT * FROM Subscriptions WHERE Id = ?").get(channelId);
 }
 
-const subDetailsByYoutubeIdStatement = db.prepare("SELECT * FROM Subscriptions WHERE YoutubeId = ?");
 export async function getSubscriptionByYoutubeId(youtubeId) {
-    return subDetailsByYoutubeIdStatement.get(youtubeId);
+    return prepare("SELECT * FROM Subscriptions WHERE YoutubeId = ?").get(youtubeId);
 }
 
-const addSubStatement = db.prepare("INSERT INTO Subscriptions(YoutubeId, Kind, Title, IconURL) VALUES (?, ?, ?, ?)");
 export async function addSubscription(youtubeId, kind, title, iconURL) {
-    const res = addSubStatement.run(youtubeId, kind, title, iconURL);
+    const res = prepare("INSERT INTO Subscriptions(YoutubeId, Kind, Title, IconURL) VALUES (?, ?, ?, ?)")
+        .run(youtubeId, kind, title, iconURL);
     return res.lastInsertRowid;
 }
 
-const updateIconStatement = db.prepare("UPDATE Subscriptions SET IconURL = ? WHERE YoutubeId = ?");
 export async function updateSubscriptionIcon(youtubeId, iconUrl) {
-    updateIconStatement.run(iconUrl, youtubeId); 
+    prepare("UPDATE Subscriptions SET IconURL = ? WHERE YoutubeId = ?").run(iconUrl, youtubeId); 
 }
 
 export async function removeSubscription(id) {
@@ -65,113 +69,106 @@ export async function removeSubscription(id) {
     //TODO: sql queries
 }
 
-const isSubStatement = db.prepare("SELECT COUNT(*) AS Result FROM Subscriptions WHERE YoutubeId = ?");
 export async function isSubscribedTo(youtubeId) {
-    return isSubStatement.get(youtubeId).Result != 0;
+    return prepare("SELECT COUNT(*) AS Result FROM Subscriptions WHERE YoutubeId = ?").get(youtubeId).Result != 0;
 }
 
-const subscribeUserToStatement = db.prepare("INSERT INTO UserSubscriptions(UserId, ChannelId, Favorite) VALUES(?, ?, FALSE)")
 export async function subscribeUserTo(userId, channelId) {
-    subscribeUserToStatement.run(userId, channelId);
+    prepare("INSERT INTO UserSubscriptions(UserId, ChannelId, Favorite) VALUES(?, ?, FALSE)").run(userId, channelId);
 }
 
-const listVideoStatement = db.prepare("SELECT * FROM Videos WHERE SubscriptionId = ? ORDER BY UploadDate DESC");
-const listViewedVideoStatement = db.prepare("SELECT * FROM Videos WHERE SubscriptionId = ? AND Viewed = ? ORDER BY UploadDate DESC");
 export async function listVideosForSubscription(channelId, viewedCondition) {
     if (viewedCondition === null) {
-        return listVideoStatement.all(channelId);
+        return prepare("SELECT * FROM Videos WHERE SubscriptionId = ? ORDER BY UploadDate DESC").all(channelId);
     } else {
         // The viewed * 1 is because better-sqlite3 does not handle booleans
-        return listViewedVideoStatement.all(channelId, viewedCondition * 1);
+        return db.prepare(`
+            SELECT * FROM Videos 
+            WHERE SubscriptionId = ? AND Viewed = ? 
+            ORDER BY UploadDate DESC
+        `).all(channelId, viewedCondition * 1);
     }
 }
 
-const listRecentVidFavStatement = db.prepare("SELECT Videos.* FROM Videos INNER JOIN Subscriptions ON Videos.SubscriptionId = Subscriptions.Id WHERE Viewed = FALSE AND Subscriptions.IsFavorite = ? ORDER BY Videos.UploadDate DESC LIMIT ?;");
 export async function listRecentVideos(favorites, limit) {
     // The viewed * 1 is because better-sqlite3 does not handle booleans
-    return listRecentVidFavStatement.all(favorites * 1, limit);
+    return prepare(`
+        SELECT Videos.* 
+        FROM Videos INNER JOIN Subscriptions ON Videos.SubscriptionId = Subscriptions.Id 
+        WHERE Viewed = FALSE AND Subscriptions.IsFavorite = ? 
+        ORDER BY Videos.UploadDate DESC LIMIT ?;
+    `).all(favorites * 1, limit);
 }
 
 
-const containsVideoStatement = db.prepare("SELECT COUNT(*) AS Result FROM Videos WHERE YoutubeId = ?");
 export async function hasVideo(youtubeId) {
-    return containsVideoStatement.get(youtubeId).Result != 0;
+    return prepare("SELECT COUNT(*) AS Result FROM Videos WHERE YoutubeId = ?").get(youtubeId).Result != 0;
 }
 
-const addVideoStatement = db.prepare("INSERT INTO Videos (YoutubeId, Title, DurationSec, Details, UploadDate, ThumbnailURL, SubscriptionId, Viewed, ViewDate) " +
-    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?); ")
 export async function addVideo(youtubeId, title, durationSec, details, uploadDate, thumbnailURL, subscriptionId, viewed) {
     // The viewed * 1 is because better-sqlite3 does not handle booleans
-    const res = addVideoStatement.run(youtubeId, title, durationSec, details, uploadDate.toISOString(), thumbnailURL, 
-        subscriptionId, viewed * 1, null); 
-    return res.lastInsertRowid;
+    return prepare(`
+        INSERT INTO Videos (YoutubeId, Title, DurationSec, Details, UploadDate, ThumbnailURL, SubscriptionId, Viewed, ViewDate) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `).run(youtubeId, title, durationSec, details, uploadDate.toISOString(), thumbnailURL, subscriptionId, viewed * 1, null)
+    .lastInsertRowid; 
 }
 
-const markViewedStatement = db.prepare("UPDATE Videos SET Viewed = ?, ViewDate = ? WHERE Id = ?");
 export async function setViewed(videoId, viewed, viewDate) {
-    markViewedStatement.run(viewed * 1, viewDate != null ? viewDate.toISOString() : null, videoId);
+    prepare("UPDATE Videos SET Viewed = ?, ViewDate = ? WHERE Id = ?").run(viewed * 1, viewDate != null ? viewDate.toISOString() : null, videoId);
 }
 
-const markFavStatement = db.prepare("UPDATE Subscriptions SET IsFavorite = ? WHERE Id = ?")
 export async function setChannelFavorite(channelId, favorite) {
-    markFavStatement.run(favorite * 1, channelId);
+    prepare("UPDATE Subscriptions SET IsFavorite = ? WHERE Id = ?").run(favorite * 1, channelId);
 }
 
-const addAnimeStatement = db.prepare("INSERT INTO Animes(MalId, Title, NbEpisodes, Genres, Viewed, NotInterested, ViewDate, ThumbnailURL, CurrentStatus, Synopsis) VALUES (?,?,?,?,?,?,?,?,?,?);")
 export async function addAnime(malId, title, nbEpisodes, genres, thumbnailURL, currentStatus, synopsis) {
-    const res = addAnimeStatement.run(malId, title, nbEpisodes, genres.join(","), 0, 0, null, thumbnailURL, currentStatus, synopsis);
-    return res.lastInsertRowid;
+    return prepare(`
+        INSERT INTO Animes(MalId, Title, NbEpisodes, Genres, Viewed, NotInterested, ViewDate, ThumbnailURL, CurrentStatus, Synopsis) VALUES (?,?,?,?,?,?,?,?,?,?)
+    `).run(malId, title, nbEpisodes, genres.join(","), 0, 0, null, thumbnailURL, currentStatus, synopsis)
+    .lastInsertRowid;
 }
 
-const markAnimeWatchedStatement = db.prepare("UPDATE Animes SET Viewed = ?, ViewDate = ? WHERE Id = ?");
 export async function markAnimeViewed(animeId, viewed, viewDate) {
-    markAnimeWatchedStatement.run(viewed * 1, viewDate != null ? viewDate.toISOString() : null, animeId);
+    prepare("UPDATE Animes SET Viewed = ?, ViewDate = ? WHERE Id = ?")
+        .run(viewed * 1, viewDate != null ? viewDate.toISOString() : null, animeId);
 }
 
-const markAnimeInterestStatement = db.prepare("UPDATE Animes SET NotInterested = ? WHERE Id = ?");
 export async function markAnimeInterest(animeId, interested) {
-    markAnimeInterestStatement.run(1 - interested * 1, animeId);
+    prepare("UPDATE Animes SET NotInterested = ? WHERE Id = ?").run(1 - interested * 1, animeId);
 }
 
-const listViewedAnimesStatement = db.prepare("SELECT * FROM Animes WHERE Viewed = TRUE");
 export async function listViewedAnimes() {
-    return listViewedAnimesStatement.all();
+    return prepare("SELECT * FROM Animes WHERE Viewed = TRUE").all();
 }
 
-const listUpcomingAnimesStatement = db.prepare("SELECT * FROM Animes WHERE Viewed = FALSE AND CurrentStatus != 'Completed'");
 export async function listUpcomingAnimes() {
-    return listUpcomingAnimesStatement.all();
+    return prepare("SELECT * FROM Animes WHERE Viewed = FALSE AND CurrentStatus != 'Completed'").all();
 }
 
-const listSuggestedAnimesStatement = db.prepare("SELECT * FROM Animes WHERE Viewed = FALSE AND CurrentStatus = 'Completed'");
 export async function listSuggestedAnimes() {
-    return listSuggestedAnimesStatement.all();
+    return prepare("SELECT * FROM Animes WHERE Viewed = FALSE AND CurrentStatus = 'Completed'").all();
 }
 
-const updateAnimeStatusStatement = db.prepare("UPDATE Animes SET CurrentStatus = ? WHERE Id = ?");
 export async function updateAnimeStatus(id, newStatus) {
-    updateAnimeStatusStatement.run(newStatus, id);
+    prepare("UPDATE Animes SET CurrentStatus = ? WHERE Id = ?").run(newStatus, id);
 }
 
-const malAnimeIsPresentStatement = db.prepare("SELECT COUNT(*) AS NbEntries FROM Animes WHERE MalId = ?")
 export async function malAnimeIsPresent(malId) {
-    return malAnimeIsPresentStatement.get(malId).NbEntries > 0;
+    return prepare("SELECT COUNT(*) AS NbEntries FROM Animes WHERE MalId = ?").get(malId).NbEntries > 0;
 }
 
-const addUserStatement = db.prepare("INSERT INTO Users(Email, PasswordHash) VALUES (?, ?)");
 export async function addUser(email, password) {
-    let res = addUserStatement.run(email, await bcrypt.hash(password, config["passwords"]["bcrypt_rounds"]));
+    let res = prepare("INSERT INTO Users(Email, PasswordHash) VALUES (?, ?)").run(email, await bcrypt.hash(password, config["passwords"]["bcrypt_rounds"]));
     return res.lastInsertRowid;
 }
 
-const userExistsStatement = db.prepare("SELECT COUNT(*) AS NbEntries FROM Users WHERE Email = ?");
 export async function userExists(email) {
-    return userExistsStatement.get(email).NbEntries > 0;
+    return prepare("SELECT COUNT(*) AS NbEntries FROM Users WHERE Email = ?").get(email).NbEntries > 0;
 }
 
-const checkUserStatement = db.prepare("SELECT * FROM Users WHERE Email = ?")
 export async function checkUserPassword(id, password) {
-    let entries = checkUserStatement.all(id); 
+    let entries = prepare("SELECT * FROM Users WHERE Email = ?").all(id); 
     if (entries.length == 0 || !await bcrypt.compare(password, entries[0].PasswordHash)) {
         return null; 
     }
--- services/subscriptions.js
+++ services/subscriptions.js
@@ -1,44 +1,44 @@
 import db from '../services/db.js';
 import yt from '../services/yt.js';
 
-export async function subscribeUserTo(userId, newSubscription) {
+export async function subscribeUserTo(userId, youtubeId) {
     let subId = null;
-    if (!await db.isSubscribedTo(newSubscription)) {
-        subId = await addSubscription(newSubscription);
+    if (!await db.isSubscribedTo(youtubeId)) {
+        subId = await addSubscription(youtubeId);
     }
     else {
-        subId = await db.getSubscriptionByYoutubeId(newSubscirption).Id;
+        subId = await db.getSubscriptionByYoutubeId(youtubeId).Id;
     }
     await db.subscribeUserTo(userId, subId);
 }
 
-export async function addSubscription(newSubscription) {
-    if (await db.isSubscribedTo(newSubscription)) {
+export async function addSubscription(youtubeId) {
+    if (await db.isSubscribedTo(youtubeId)) {
         throw { status: 400, details: "Already subscribed to channel/playlist" };
     }
 
     var playlistId = null;
     var subId = null;
-    if (newSubscription.startsWith("UC")) {
-        console.log(`Adding subscription to channel ${newSubscription}`);
-        const channelInfo = await yt.getChannelInfos(newSubscription);
-        subId = await db.addSubscription(newSubscription, "Channel", channelInfo.snippet.title, 
+    if (youtubeId.startsWith("UC")) {
+        console.log(`Adding subscription to channel ${youtubeId}`);
+        const channelInfo = await yt.getChannelInfos(youtubeId);
+        subId = await db.addSubscription(youtubeId, "Channel", channelInfo.snippet.title, 
             channelInfo.snippet.thumbnails.medium.url);
         playlistId = channelInfo.contentDetails.relatedPlaylists.uploads;
-    } else if (newSubscription.startsWith("PL")) {
-        console.log(`Adding subscription to playlist ${newSubscription}`);
-        playlistId = newSubscription;
+    } else if (youtubeId.startsWith("PL")) {
+        console.log(`Adding subscription to playlist ${youtubeId}`);
+        playlistId = youtubeId;
         const playlistInfo = await yt.getPlaylistInfos(playlistId);
-        subId = await db.addSubscription(newSubscription, "Playlist", playlistInfo.snippet.title, playlistInfo.snippet.thumbnails.medium.url);
+        subId = await db.addSubscription(youtubeId, "Playlist", playlistInfo.snippet.title, playlistInfo.snippet.thumbnails.medium.url);
     } else {
         throw { status: 400, details: "Invalid subscription id" };
     }
 
     for (var videoInfo of await yt.getPlaylistVideos(playlistId, true)) {
-        //TODO: handle better if viewed or not.
+        //TODO: handle better short videos.
         //TODO: make sure video does not exist yet
         if (videoInfo.additionalDetails.duration < 3 * 60) {
-            //Skip already viewed videos
+            //Skip short videos
             continue;
         }
         await db.addVideo(videoInfo.contentDetails.videoId, videoInfo.snippet.title, videoInfo.additionalDetails.duration, 
